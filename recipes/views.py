c937175c209530e2daffa264a5ad80bf83e52a41
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-04 19:02:03 -0300

    created app recipes

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def about(request):
+    return HttpResponse('ABOUT')
+
+
+def contact(request):
+    return HttpResponse('CONTACT')
