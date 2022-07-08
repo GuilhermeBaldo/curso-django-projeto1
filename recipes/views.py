71fbf85ed0228606134f991e529f19570bdcef2a
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-08 10:04:23 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def about(request):
-    return HttpResponse('ABOUT')
-
-
-def contact(request):
-    return HttpResponse('CONTACT')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Guilherme Baldo Carlos'
+    })
